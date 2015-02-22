953c5d4d31c3dfc06993bde17e005f1da0ae852a
Klint Tiley <kctiley@gmail.com>
2015-02-22 14:29:24 -0700

    created  mad_libs.rb
    [#88836348]

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+puts 'Puts give me an adjective:'
+adjective = gets.chomp
+
+puts 'Give me another adjective:'
+another_adjective = gets.chomp
+
+puts 'Give me a noun:'
+noun = gets.chomp
+
+puts 'Give me another noun:'
+another_noun = gets.chomp
+
+puts 'Give me a plural noun:'
+plural_noun = gets.chomp
+
+puts 'Give me a game:'
+game = gets.chomp
+
+puts 'Give me a another plural noun:'
+another_plural_noun = gets.chomp
+
+puts "Give me a verb ending in 'ing':"
+verb_ing = gets.chomp
+
+puts "Give me another verb ending in 'ing':"
+another_verb_ing = gets.chomp
+
+puts 'Give me another plural noun:'
+another_plural_noun_2 = gets.chomp
+
+puts "Give me another verb ending in 'ing':"
+another_verb_ing_2 = gets.chomp
+
+puts 'Give me another noun:'
+another_noun_2 = gets.chomp
+
+puts 'Give me a plant:'
+plant = gets.chomp
+
+puts 'Give me a part of the body:'
+part_of_the_body = gets.chomp
+
+puts 'Give me a place:'
+place = gets.chomp
+
+puts "Give me another verb ending in 'ing':"
+another_verb_ing_3 = gets.chomp
+
+puts 'Give me another adjective:'
+another_adjective_2 = gets.chomp
+
+puts 'Give me a number:'
+number = gets.chomp.to_s
+
+puts 'Give me another plural noun:'
+another_plural_noun_3 = gets.chomp
+
+puts 'Here is your madlib:'
+puts "A vacation is when you take a trip to some #{adjective} place"
+puts "with your #{another_adjective} family. Usually you go to some place"
+puts "that is near a/an #{noun} or up on a/an #{another_noun}."
+puts "A good vacation place is one where you can ride #{plural_noun}"
+puts "or play #{game} or go hunting for #{another_plural_noun}. I like"
+puts "to spend my time #{verb_ing} or #{another_verb_ing}."
+puts 'When parents go on a vacation, they spend their time eating'
+puts "three #{another_plural_noun_2} a day, and fathers play golf, and mothers"
+puts "sit around #{another_verb_ing_2}. Last summer, my little brother"
+puts "fell in a/an #{another_noun_2} and got poison #{plant} all"
+puts "all over his #{part_of_the_body}. My family is going to go to (the)"
+puts "#{place}, and I will practice #{another_verb_ing_3}. Parents"
+puts 'need vacations more than kids because parents are always very'
+puts "#{another_adjective_2} and because they have to work #{number}"
+puts "hours every day all year making enough #{another_plural_noun_3} to pay"
+puts 'for the vacation.'
