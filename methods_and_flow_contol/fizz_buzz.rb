249054e25ba0ee8c885c9fc6af775fec6e2ec50e
Klint Tiley <kctiley@gmail.com>
2015-02-24 23:10:47 -0700

    Made corrections after realizing that I forgot to run Rubocop.
    [#88878246]

--- methods_and_flow_contol/fizz_buzz.rb
+++ methods_and_flow_contol/fizz_buzz.rb
@@ -2,13 +2,13 @@
 
 number = 1
 
-while number != 101 do
+while number != 101
 
-  if number%3 == 0 && number%5 != 0
+  if number % 3 == 0 && number % 5 != 0
     puts 'Fizz'
-  elsif number%5 == 0 && number%3 != 0
+  elsif number % 5 == 0 && number % 3 != 0
     puts 'Buzz'
-  elsif number%3 == 0 && number%5 == 0
+  elsif number % 3 == 0 && number % 5 == 0
     puts 'FizzBuzz'
   else
     puts number
