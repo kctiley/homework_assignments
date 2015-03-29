d1f00abc6f165d9a11b45f6df59b0b03f28630bd
Klint Tiley <kctiley@gmail.com>
2015-03-29 13:52:03 -0600

    Changed design of program to be recursive

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,31 +1,48 @@
 #!/usr/bin/env ruby
 
-bottles_of_beer = 99
+# bottles_of_beer = 99
+#
+# while bottles_of_beer > 2
+#
+#   puts "#{bottles_of_beer} bottles of beer on the wall, #{bottles_of_beer}"\
+#   ' bottles of beer.'
+#   bottles_of_beer -= 1
+#   puts "Take one down and pass it around, #{bottles_of_beer} bottles of beer"\
+#   ' on the wall.'
+#   puts ' '
+#
+# end
+#
+# puts "#{bottles_of_beer} bottles of beer on the wall, #{bottles_of_beer}"\
+# ' bottles of beer.'
+# bottles_of_beer -= 1
+# puts "Take one down and pass it around, #{bottles_of_beer} bottle of beer"\
+# ' on the wall.'
+#
+# puts ' '
+#
+# puts "#{bottles_of_beer} bottle of beer on the wall, #{bottles_of_beer}"\
+# ' bottle of beer.'
+# puts 'Take one down and pass it around, no more bottles of beer on the wall.'
+#
+# puts ' '
+#
+# puts 'No more bottles of beer on the wall, no more bottles of beer.'
+# puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
 
-while bottles_of_beer > 2
-
-  puts "#{bottles_of_beer} bottles of beer on the wall, #{bottles_of_beer}"\
-  ' bottles of beer.'
-  bottles_of_beer -= 1
-  puts "Take one down and pass it around, #{bottles_of_beer} bottles of beer"\
-  ' on the wall.'
-  puts ' '
-
+def bottles_plural_or_singular(number)
+  number == 1 ? "#{number} bottle" : "#{number} bottles"
 end
 
-puts "#{bottles_of_beer} bottles of beer on the wall, #{bottles_of_beer}"\
-' bottles of beer.'
-bottles_of_beer -= 1
-puts "Take one down and pass it around, #{bottles_of_beer} bottle of beer"\
-' on the wall.'
+def bottles_of_beer (number)
+  while number > 1
+    puts "#{bottles_plural_or_singular(number)} of beer on the wall, #{bottles_plural_or_singular(number)} bottles of beer."
+    number = number - 1
+    puts "Take one down and pass it around, #{bottles_plural_or_singular(number)} of beer on the wall."
+  end
 
-puts ' '
+  puts 'No more bottles of beer on the wall, no more bottles of beer.'
+  puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+end
 
-puts "#{bottles_of_beer} bottle of beer on the wall, #{bottles_of_beer}"\
-' bottle of beer.'
-puts 'Take one down and pass it around, no more bottles of beer on the wall.'
-
-puts ' '
-
-puts 'No more bottles of beer on the wall, no more bottles of beer.'
-puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+bottles_of_beer(99)
