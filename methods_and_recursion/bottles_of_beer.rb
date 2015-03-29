08a85af0ccde0c3f05e94b9b8660de029b496b44
Klint Tiley <kctiley@gmail.com>
2015-03-29 15:13:27 -0600

    Modified to call method within method.

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -28,21 +28,60 @@
 # puts ' '
 #
 # puts 'No more bottles of beer on the wall, no more bottles of beer.'
-# puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+# puts 'Go to the store and buy some more, 99 bottles of beer on the wall.
 
+# VERSION 2
+# def bottles_plural_or_singular(number)
+#   number == 1 ? "#{number} bottle" : "#{number} bottles"
+# end
+#
+# def bottles_of_beer (number)
+#   while number > 1
+#     puts "#{bottles_plural_or_singular(number)} of beer on the wall, #{bottles_plural_or_singular(number)} bottles of beer."
+#       number = number - 1
+#     puts "Take one down and pass it around, #{bottles_plural_or_singular(number)} of beer on the wall."
+#   end
+#
+#   puts 'No more bottles of beer on the wall, no more bottles of beer.'
+#   puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+# end
+#
+# bottles_of_beer(99)
+
+# VERSION 3
+# def bottles(number)
+#
+#   if number == 0
+#     puts "nomore"
+#     return
+#   else
+#     puts "#{number}"
+#   end
+#
+#   bottles(number - 1)
+# end
+#
+# bottles(99)
+
+# VERSION 4
 def bottles_plural_or_singular(number)
   number == 1 ? "#{number} bottle" : "#{number} bottles"
 end
 
 def bottles_of_beer (number)
-  while number > 1
-    puts "#{bottles_plural_or_singular(number)} of beer on the wall, #{bottles_plural_or_singular(number)} bottles of beer."
-    number = number - 1
-    puts "Take one down and pass it around, #{bottles_plural_or_singular(number)} of beer on the wall."
+  if number > 0
+    puts "#{bottles_plural_or_singular(number)} of beer on the wall, #{bottles_plural_or_singular(number)} of beer."
+    puts "Take one down and pass it around, #{bottles_plural_or_singular(number - 1)} of beer on the wall."
+  else
+    puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    return
+
   end
 
-  puts 'No more bottles of beer on the wall, no more bottles of beer.'
-  puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+  bottles_of_beer(number - 1)
+
+
 end
 
 bottles_of_beer(99)
